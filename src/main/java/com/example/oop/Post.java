850407c23c3ebafc6381854149facc1f59dfde54
ali-elbagoury <110342114+DopaminoZ@users.noreply.github.com>
2024-05-25 05:31:00 +0300

    post class

--- src/main/java/com/example/oop/Post.java
+++ src/main/java/com/example/oop/Post.java
@@ -1,6 +1,24 @@
 package com.example.oop;
+import javafx.scene.image.Image;
 import javafx.scene.media.*;
+
+import java.util.ArrayList;
+
 public class Post {
+    private User owner;
     private String caption;
-    private Media content;
+    private Media vid;
+    private Image image;
+    private ArrayList<User> likes = new ArrayList<>();
+
+    public Post(User owner, String caption, Media vid) {
+        this.caption = caption;
+        this.vid = vid;
+        image = null;
+    }
+    public Post(User owner, String caption, Image img) {
+        this.caption = caption;
+        this.image = img;
+        vid = null;
+    }
 }
